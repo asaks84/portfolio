baa3c2900b1998aa410257f602ff13ade941f85d
Cauê Carrera <cauecpl@gmail.com>
2022-07-11 23:25:36 -0300

    Header / Nav menu / Logo done.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,31 @@
 import 'animate.css';
 import 'reset-css';
 import '../css/style.scss';
+import '@mdi/font/css/materialdesignicons.min.css';
+
+// group of functions to set a given theme/color-scheme
+(() => {
+  function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+  }
+
+  // function to toggle between light and dark theme
+  // eslint-disable-next-line no-unused-vars
+  function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('default');
+    } else {
+      setTheme('dark');
+    }
+  }
+
+  // Immediately invoked function to set the theme on initial load
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('dark');
+    document.getElementById('slider').checked = false;
+  } else {
+    setTheme('default');
+    document.getElementById('slider').checked = true;
+  }
+})();
