cc97595832a9c270bcceae1d6cafbe06c881f356
Cauê Carrera <cauecpl@gmail.com>
2022-10-18 11:58:03 -0300

    open/close modal on mobile fixed

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,29 +1,27 @@
-import { fadeIn, fadeOut } from './fadeEffect';
-
 const close = document.querySelector('#modal .close');
 const modalWrap = document.querySelector('.modal-wrap');
-const portfolio = document.querySelectorAll('.description');
+const btnOpen = document.querySelectorAll('.description .btn');
+const carouselWindow = document.querySelector('#modal');
 
+const slideWidth = parseFloat(window.getComputedStyle(carouselWindow).width);
+
+// OPEN/CLOSE PORTFOLIO
 function showModal() {
-  fadeIn(modalWrap);
+  modalWrap.classList.add('visible');
 }
 
 function closeModal(e) {
   if (e.target.classList.contains('close')) {
-    fadeOut(modalWrap);
+    modalWrap.classList.remove('visible');
   }
 }
-
-portfolio.forEach((e) => e.addEventListener('click', showModal));
+btnOpen.forEach((e) => e.addEventListener('click', showModal));
+btnOpen.forEach((e) => e.addEventListener('touchend', showModal));
 close.addEventListener('click', closeModal);
 modalWrap.addEventListener('click', closeModal);
 
-const carouselDiv = document.querySelector('.carousel');
-
-const slideWidth = 700;
-// const threshold = slideWidth / 3;
-
 function shiftSlide(direction) {
+  const carouselDiv = document.querySelector('.carousel');
   const slideFirst = document.querySelector('.carousel > div:first-child');
   const slideLast = document.querySelector('.carousel > div:last-child');
 
